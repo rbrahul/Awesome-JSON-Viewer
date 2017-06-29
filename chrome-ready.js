ed8436842df192305c3ebed216c0f41201a5413c
rbrahul <rahulbaruri1@gmail.com>
2017-06-29 23:17:31 +0600

    Rewritten Chrome-ready command

--- chrome-ready.js
+++ chrome-ready.js
@@ -32,6 +32,8 @@ exec("mv "+__dirname+"/build/background.js "+__dirname+"/build/static/js/backgro
     }
 });
 
+
+
 exec("rm -f "+__dirname+"/build/static/js/main*.js.map ", function (error, stdout, stderr) {
     if (error) {
         console.log('exec error: ' + error);
@@ -47,5 +49,53 @@ exec("rm -f "+__dirname+"/build/static/css/main*.css.map ", function (error, std
         console.log('exec error: ' + error);
     } else{
         console.log("Main*.css.map file has been deleted successfully");
+    }
+});
+
+exec("mv "+__dirname+"/build/css/*.css "+__dirname+"/build/static/css", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("All files in css folder moved to static/css");
+    }
+});
+
+exec("mkdir "+__dirname+"/build/static/css/themes", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("Created static/css/themes");
+    }
+});
+
+exec("mv "+__dirname+"/build/css/themes/*.css "+__dirname+"/build/static/css/themes", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("All files in css/themes folder moved to static/css/themes");
+    }
+});
+
+exec("mv "+__dirname+"/build/js/*.js "+__dirname+"/build/static/js", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("All files in js folder moved to static/js");
+    }
+});
+
+exec("rm -rf "+__dirname+"/build/js", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("Js folder has been deleted");
+    }
+});
+
+exec("rm -rf "+__dirname+"/build/css", function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    } else{
+        console.log("CSS folder has been deleted");
     }
 });
