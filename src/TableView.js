6b0078e8810678da8ab6bff0f620f41ddae8251b
Dominik Bauernfeind <d.bauernfeind@linemetrics.com>
2018-09-29 23:24:07 +0200

    add rendering of json as table, add styling

--- src/TableView.js
+++ src/TableView.js
@@ -1,24 +1,79 @@
 import React, { Component } from 'react';
 import './utils/json-viewer/jquery.json-viewer.css';
+import './css/TableView.css';
 
 class TableView extends Component {
-    constructor (props) {
-        super(props);
+
+    /**
+     * Iterate over each element in the Object, return it rendered as table.
+     * Each value is rendered type specifically or recursively if it is an object on its own.
+     *
+     * @param obj
+     * @returns {*}
+     */
+    renderObject (obj) {
+        const rows = Object.entries(obj).map(o => {
+            const key = o[0],
+                value = o[1];
+            return (
+                <tr key={key}>
+                    <th>{key}</th>
+                    {this.renderCell(value)}
+                </tr>
+            )
+        });
+
+        return (
+            <table>
+                {rows}
+            </table>
+        )
     }
 
-    componentDidMount () {
+    /**
+     * Render each value type specific and return a <td> tag.
+     *
+     * @param value
+     * @returns {*}
+     */
+    renderCell (value) {
+        if (typeof value === 'string' || typeof value === 'number') {
+            return (<td className="value">{value || '" "'}</td>);
+        }
 
+        if (typeof value === 'boolean') {
+            return (<td className="value">{value ? 'true' : 'false'}</td>)
+        }
+
+        if (Array.isArray(value)) {
+            return (
+                <td>{this.renderArrayAsTable(value)}</td>
+            );
+        }
+
+        return (<td>{this.renderObject(value)}</td>);
     }
 
-    componentWillUnmount () {
+    /**
+     * Arrays are simply rows of value specific cells, wrapped in a <table> for highlighting.
+     *
+     * @param value
+     * @returns {*}
+     */
+    renderArrayAsTable (value) {
+        const rows = value.map((content, index) => (<tr key={index}>{this.renderCell(content)}</tr>));
+
+        return (
+            <table>
+                {rows.length > 0 ? rows : <tr>
+                    <th className="json-empty">Empty array</th>
+                </tr>}
+            </table>
+        )
     }
 
     render () {
-        return (
-            <div>
-                nice
-            </div>
-        );
+        return this.renderObject(this.props.json);
     }
 }
 
